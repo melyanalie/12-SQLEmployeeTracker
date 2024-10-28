95fca7dad667194feabbdf03ed427495c9d966a1
melyanalie <melyana.lie86@gmail.com>
2024-10-28 15:43:51 -0400

    fixed schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,19 +1,20 @@
-CREATE TABLE departments (
+CREATE TABLE department (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
+    name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE roles (
+CREATE TABLE role (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(100) NOT NULL,
-    department_id INTEGER REFERENCES departments(id),
-    salary DECIMAL NOT NULL
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    
 );
 
-CREATE TABLE employees (
+CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-    role_id INTEGER REFERENCES roles(id),
-    manager_id INTEGER REFERENCES employees(id)
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER
 );
